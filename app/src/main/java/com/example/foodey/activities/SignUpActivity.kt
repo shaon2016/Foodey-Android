386eb8479816afe99a5b777ce338a8f21643d91e
Shaon <shaondeveloper@gmail.com>
2019-09-19 20:09:06 +0600

    Sign up design done

---
+++ app/src/main/java/com/example/foodey/activities/SignUpActivity.kt
@@ -0,0 +1,13 @@
+package com.example.foodey.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.foodey.R
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
